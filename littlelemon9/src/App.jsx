11188c03aab2fd34015872831493bc9f6119068a
UX SCOUTS <admin@uxscouts.com>
2026-03-28 12:44:36 +0000

    Navigation page content moved to App.jsx to bypass this uneccessary nesting

--- littlelemon9/src/App.jsx
+++ littlelemon9/src/App.jsx
@@ -1,18 +1,78 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
-import TestNav from './components/TestNav';
+import { useState } from 'react';
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+
 import Footer from './components/layout/Footer.jsx';
+
+import Homepage from './components/layout/Homepage.jsx';
+import AboutUsMod from './components/layout/AboutUsMod.jsx';
+import Reservation from './components/layout/Reservation.jsx';
+import BookingPage from './components/layout/BookingPage.jsx';
+import HomeOrder from './components/layout/HomeOrder.jsx';
+
+
+import LemonLogo from './assets/images/LemonLogo.png';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const Home = () => <Homepage/>;
+const About = () => <AboutUsMod/>;
+const ReservationMod = () => <Reservation/>;
+const HomeOrderMod = () => <HomeOrder/>;
+const Login = () => <h2>Login</h2>;
+
+/*
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+*/
+
 
 function App() {
   return (
     <>
-    <Navigation/>
+      <Router>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+          <Link class="nav-link navbar-brand" to="/">
+            <img src={LemonLogo} width="250" alt="Little Lemon Logo" />
+          </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+              <Link class="nav-link" to="/">Home</Link>
+              <Link class="nav-link" to="/about">About Us</Link>
+              <Link class="nav-link" to="/reservation">Reservation</Link>
+              <Link class="nav-link" to="/homeorder">Home Order</Link>
+          </Nav>
+          <Nav className="mr">
+              <Link class="nav-link text-end" to="/login">
+                <Button variant="warning">Login</Button>
+              </Link>
+          </Nav>          
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/reservation" element={<ReservationMod />} />
+          <Route path="/homeorder" element={<HomeOrderMod />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
       <Footer/> 
     </>
   );
-}
-
-
+};
 
 export default App;
+
 
